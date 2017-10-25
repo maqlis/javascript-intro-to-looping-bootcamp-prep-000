03a91c6c66b236e0b6cdc39170ae6395bc4263ab
Małgorzata Klisińska <klis.mal@gmail.com>
2017-10-25 12:11:26 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -11,6 +11,11 @@ function whileLoop(n){
   }
   return 'done'
 }
+function maybeTrue() {
+  return Math.random() >= 0.5 // Returns a random number between 0 (inclusive) and 1 (exclusive)
+}
 function doWhileLoop(array){
-  
+  do{
+    array.shift()
+  }while(array.length > 0 && maybeTrue())
 }
